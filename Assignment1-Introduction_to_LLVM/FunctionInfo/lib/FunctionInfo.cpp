5337eb928a5105b5a62a15b203c8f7c95615d529
liusy58 <mg21330037@smail.nju.edu.cn>
2022-04-11 14:06:26 +0800

    update on 2022-04-11 14:06:26

--- Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
@@ -1,7 +1,10 @@
 #include <llvm/IR/Module.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/Pass.h>
 #include <llvm/Support/raw_ostream.h>
 
+#include <string>
+#include <map>
 using namespace llvm;
 
 namespace {
@@ -9,6 +12,16 @@ namespace {
 class FunctionInfo final : public ModulePass {
 public:
   static char ID;
+
+  class Info{
+    public:
+      int args;
+      int calls;
+      int blocks;
+      int insts;
+  };
+
+  std::map<std::string, Info> infos;
 
   FunctionInfo() : ModulePass(ID) {}
 
@@ -21,11 +34,40 @@ public:
     outs() << "CSCD70 Function Information Pass"
            << "\n";
 
+    for(auto &func:M){
+      auto name = func.getName().str();
+      infos[name] = Info();
+
+    }
+
     /**
      * @todo(cscd70) Please complete this method.
      */
 
     return false;
+  }
+
+  void iterate_function(Function&func){
+    auto name = func.getName().str();
+    infos[name].args = func.arg_size();
+    infos[name].blocks = func.getBasicBlockList().size();
+    for(auto&bb:func){
+      infos[name].insts += iterate_block(bb);
+    }
+  }
+
+
+//return instruction_num;
+  int iterate_block(BasicBlock&block){
+    int inst_num = 0;
+    for(auto&inst:block){
+      if(CallInst* call_inst = dyn_cast<CallInst>(&inst)){
+        auto name = call_inst->getCalledFunction()->getName().str();
+        infos[name].calls+=1;
+        inst_num++;
+      }
+    }
+    return inst_num;
   }
 }; // class FunctionInfo
 
